d6d3ee69da4e5804bf2e6f06e1f058ac9cc43ed8
PeterKellett <peterwkellett@gmail.com>
2020-07-03 13:15:37 +0000

    Added templates

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env as config
+
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = 'aroundTheWorldRace'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello world...again!...and again!!'
+@app.route('/home')
+def home():
+    return render_template("home.html", clues=mongo.db.clues.find())
 
 
 if __name__ == '__main__':
