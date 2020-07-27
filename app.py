58c3fc4fc85a20d62ac1ce890bfa3a49eba5050f
PeterKellett <peterwkellett@gmail.com>
2020-07-27 21:12:27 +0000

    Rendered google maps success

--- app.py
+++ app.py
@@ -18,8 +18,24 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html", clues=mongo.db.clues.find())
+    return render_template("home.html",
+                           clues=mongo.db.clues.find({"leg": "1"}).sort("step"),
+                           teams=mongo.db.teams.find().sort("time"))
 
+
+@app.route('/join_game', methods=["POST"])
+def join_game():
+    teams = mongo.db.teams
+    team_doc = {'team_name': request.form.get('team_name'), 'time': '000'}
+    teams.insert_one(team_doc)
+    return redirect(url_for('stage_1'))
+
+
+@app.route('/stage_1')
+def stage_1():
+    return render_template("stage-1.html",
+                           clues=mongo.db.clues.find({"leg": "1"}).sort("step"),
+                           teams=mongo.db.teams.find().sort("time"))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
